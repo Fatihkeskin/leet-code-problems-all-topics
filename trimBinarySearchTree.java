e949e73f4f216c549dc24129d0810d159c33beaa
Fatih Keskin <fatihkeskin7@outlook.com>
2022-04-15 23:22:37 +0300

    Trim Binary Search Tree Problem

---
+++ trimBinarySearchTree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        TreeNode tempNode = root;
+        
+        while (tempNode != null) {
+            if (tempNode.val < low && tempNode.val > high) {
+                tempNode = deleteNode(tempNode);
+            } else {
+                
+            }
+            
+        }
+        
+        return tempNode;
+    }
+}

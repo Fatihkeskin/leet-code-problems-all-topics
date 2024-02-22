6765a6c696f7e5fefde43b67f1bec3a4c2eeb569
Fatih Keskin <fatihkeskin7@outlook.com>
2024-02-22 10:06:39 +0100

    Create twoSums.java

---
+++ twoSums.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {3,3};
+        int target = 6;
+        Main main = new Main();
+        Arrays.stream(main.twoSum(nums, target)).sorted().forEach((i) -> System.out.println(i));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] + nums[j] == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                }
+            }
+        }
+        return answer;
+    }
+}
